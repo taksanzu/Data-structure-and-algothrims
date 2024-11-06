29766b03c706f4f1fd4fa189f2e9fe8d845f1fcb
Trương An Khang <ankhang897979@gmail.com>
2024-11-06 14:12:14 +0700

    Radix Sort

---
+++ RadixSort/src/main/java/org/tak/Main.java
@@ -0,0 +1,66 @@
+package org.tak;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("The array before sorting: " + Arrays.toString(arr));
+
+        radixSort(arr);
+
+        System.out.println("The array after sorting: " + Arrays.toString(arr));
+    }
+
+    public static int getMax(int[] arr, int n) {
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void countSort(int[] arr, int n, int exp) {
+        int[] output = new int[n];
+        int[] count = new int[10];
+        Arrays.fill(count, 0);
+
+        for (int i = 0; i < n; i++) {
+            count[(arr[i] / exp) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+    }
+
+    public static void radixSort(int[] arr) {
+        int n = arr.length;
+        int m = getMax(arr, n);
+
+        for (int exp = 1; m / exp > 0; exp *= 10) {
+            countSort(arr, n, exp);
+        }
+    }
+}
